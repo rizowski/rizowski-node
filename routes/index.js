1c2e24c36a467028f221518976ffaf78ad71d178
Colten Rouska <colten.rouska@extendhealth.com>
2014-09-05 14:59:40 -0600

    Redirect to blog

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
 //  console.log(req);
-  if(req.get('host') != 'rizowski.net')
+//  if(req.get('host') != 'rizowski.net')
     //console.log(req.get('host'));
-    res.redirect(301, 'http://rizowski.net');
-  res.render('index', { title: 'Rizowski.net' });
+    res.redirect('http://blog.rizowski.net');
+//  res.render('index', { title: 'Rizowski.net' });
 });
 
 module.exports = router;
